b499d8f65aa18f785ce0496805644e2be036f39c
sathiyaseelan <sathiyaseelan2598@gmail.com>
2022-10-06 13:19:45 +0530

    two numbers add subtract multiple and divide

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+class Calculator{
+
+public static void main(String arg[]){
+    
+    Calculator calc=new Calculator();
+    calc.add();
+    calc.subtract();
+    calc.multiply();
+    calc.divide();
+}
+void add(){
+    System.out.println(10+20);
+}
+void subtract(){
+    System.out.println(10-20);
+}
+void multiply(){
+    System.out.println(10*20);
+}
+void divide(){
+    System.out.println(10/20);
+}
+
+}
