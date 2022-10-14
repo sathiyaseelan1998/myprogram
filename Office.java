ed100a60b6c6fd1313fcd37b622e038e6caee349
sathiyaseelan <sathiyaseelan2598@gmail.com>
2022-10-14 14:27:20 +0530

    package

---
+++ Office.java
@@ -0,0 +1,15 @@
+package tamilnadu.chennai;
+
+public class Office{
+public int salary(){
+    int salary=45000;
+    return salary;
+}
+public void work(){
+    System.out.println("Software Developer");
+}
+void branch(){
+    System.out.println("show only chennai package");
+}
+
+}
