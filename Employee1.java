9382835b1c19654ddd9d99be9e0cc010355a5af8
sathiyaseelan <sathiyaseelan2598@gmail.com>
2022-10-14 14:31:28 +0530

    package

---
+++ Employee1.java
@@ -0,0 +1,13 @@
+package tamilnadu.chennai;
+
+class Employee1{
+
+public static void main(String arg[]){
+
+Office of=new Office();
+int salary=of.salary();
+System.out.println("Salary: "+salary);
+of.work();
+of.branch();
+}
+}
---
+++ Employee2.java
@@ -0,0 +1,12 @@
+package tamilnadu.trichy;
+import tamilnadu.chennai.Office;
+class Employee2{
+
+public static void main(String arg[]){
+
+   Office of=new Office();
+   System.out.println("Salary: "+of.salary());
+   of.work();
+
+}
+}
