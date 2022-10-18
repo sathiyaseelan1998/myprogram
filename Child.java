0d075088b7dda7ce16e1faaedafe184b6bf1ae0e
sathiyaseelan <sathiyaseelan2598@gmail.com>
2022-10-18 13:59:01 +0530

    single inheritance

---
+++ Child.java
@@ -0,0 +1,22 @@
+class Child extends Parent{
+
+int age=25;
+String name="dhru";
+
+void salary(){
+    System.out.println(250000);
+}
+public static void main(String args[]){
+
+Child child=new Child();
+System.out.println("Name:"+child.name);
+System.out.println("Age:"+child.age);
+System.out.println("Address:"+child.age);
+
+child.salary();
+String profession=child.profession();
+System.out.println(profession);
+
+}
+
+}
---
+++ Parent.java
@@ -0,0 +1,15 @@
+class Parent{
+
+int age=53;
+String name="vikram";
+String address="chennai";
+
+void salary(){
+    System.out.println(500000);
+}
+String profession(){
+
+    return "actor,"+"singer";
+}
+
+}
